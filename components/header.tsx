8fcde463b6996c4a2994dd4db50ddfa9f22d0c33
Furkan Tanyol <furkantanyol@gmail.com>
2020-07-11 09:59:45 +1000

    Init

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,21 @@
 import Link from 'next/link'
+import React from "react";
 
 const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/">
-        <a className="hover:underline">Blog</a>
-      </Link>
-      .
+        <div className="pr-2">
+            <Link href="/">
+                <a className="hover:underline">Home</a>
+            </Link>
+            .
+        </div>
+        <div className="pr-2">
+            <Link href="/blog">
+                <a className="hover:underline">Blog</a>
+            </Link>
+            .
+        </div>
     </h2>
   )
 }
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,16 +1,19 @@
 import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
 import Post from '../types/post'
-import Header from "../components/header";
+import React from "react";
 
 type Props = {
   allPosts: Post[]
 }
 
-const Index = ({ allPosts }: Props) => {
+const Blog = ({ allPosts }: Props) => {
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(1)
   return (
@@ -19,16 +22,26 @@ const Index = ({ allPosts }: Props) => {
         <Head>
           <title>Next.js Blog Example with {CMS_NAME}</title>
         </Head>
-          <Header/>
         <Container>
-         hello
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
     </>
   )
 }
 
-export default Index
+export default Blog
 
 export const getStaticProps = async () => {
   const allPosts = getAllPosts([
